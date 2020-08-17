0d7eec02c761c42604bd2808cffe202276b8f070
Computer-CGuy <computer.guybuilder@gmail.com>
2020-08-17 14:08:57 +0530

    2D extension

--- BlenderCyclesTest.py
+++ BlenderCyclesTest.py
@@ -10,18 +10,17 @@ l = []
 
 random.seed(50)
 
-for i in range(0,5,1):
-    a,b,c,d,e = randint(0,3),randint(0,10),randint(1,4),randint(0,3),randint(0,3)
+for i in range(0,10000,1):
+    a,b,c,d = randint(0,12),randint(1,16),randint(0,4),randint(0,3)
     bpy.data.materials["Drop"].node_tree.nodes["Musgrave Texture"].inputs[1].default_value = a
     bpy.data.materials["Drop"].node_tree.nodes["Musgrave Texture"].inputs[2].default_value = b
     bpy.data.materials["Drop"].node_tree.nodes["Musgrave Texture"].inputs[3].default_value = c
     bpy.data.materials["Drop"].node_tree.nodes["Musgrave Texture"].inputs[4].default_value = d
-    bpy.data.materials["Drop"].node_tree.nodes["Musgrave Texture"].inputs[5].default_value = e
-    l.append([a,b,c,d,e])
+    l.append([a,b,c,d])
     bpy.context.scene.render.image_settings.file_format='JPEG'
-    bpy.context.scene.render.filepath = "C:/Users/My PC/Desktop/Python/NeuralRenderer/MusgraveTextureNeuralRenderer/Generator/pic%0.2d.jpg"%i
+    bpy.context.scene.render.filepath = "/content/drive/My Drive/MusgraveTextureNeuralRenderer/Generator2/pic%0.2d.jpg"%i
     bpy.ops.render.render(use_viewport = True, write_still=True)
-    with open("out.pkl","wb") as f:
+    with open("/content/drive/My Drive/MusgraveTextureNeuralRenderer/out.pkl","wb") as f:
         pickle.dump(l,f)
 #    #bpy.data.objects.remove(ob,do_unlink=True)
 
